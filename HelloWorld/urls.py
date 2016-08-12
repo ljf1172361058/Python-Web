801b9ffa5758c1260837a57daf8c83ebf6ae89ee
lizhihui <1172361058@qq.com>
2016-08-12 17:28:00 +0800

    初始化Python Web项目

---
+++ HelloWorld/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import *
+from HelloWorld.view import hello
+from HelloWorld.testdb import save
+
+
+urlpatterns = patterns("",
+                       ('^hello/$', hello),
+                       ('^save/$', save),
+                       )
