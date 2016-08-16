12f7f1a9e7c9914da7dc5a02728c84306285eab2
lizhihui <1172361058@qq.com>
2016-08-16 10:13:47 +0800

    修改两个文件

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import *
 from HelloWorld.view import hello
 from HelloWorld.testdb import save
-<<<<<<< HEAD
-from HelloWorld.wechat_python_sdk import checkout
-=======
->>>>>>> b798cb59ea60320c85ecfa754f63eeca32834ede
+
 
 
 urlpatterns = patterns("",
                        ('^hello/$', hello),
                        ('^save/$', save),
-<<<<<<< HEAD
-                       ("^checkout$", checkout),
-=======
->>>>>>> b798cb59ea60320c85ecfa754f63eeca32834ede
                        )
